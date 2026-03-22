64b01decae84828d2b6af152d4f632d510c54936
rulssss <marianopicco83@gmail.com>
2026-03-22 15:21:42 -0300

    se agrego egress, faltaba

--- aws-vpc-terraform/main.tf
+++ aws-vpc-terraform/main.tf
@@ -84,6 +84,13 @@ resource "aws_security_group" "basic" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
     tgags = {
         Name = "rulss-sg"
     }
--- aws-vpc-terraform/variables.tf
+++ aws-vpc-terraform/variables.tf
@@ -7,10 +7,10 @@ variable "vpc_cidr" {
 }
 
 variable "public_subnet_1" {
-    default = "10.0.0.0/24"
+    default = "10.0.1.0/24"
 }
 
 variable "public_subnet_2" {
-    default = "10.0.0.0/24"
+    default = "10.0.2.0/24"
 }
 

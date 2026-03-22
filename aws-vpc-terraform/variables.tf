cd857bb3fecb8c4ea8b3741873cad1903d93589b
rulssss <marianopicco83@gmail.com>
2026-03-22 12:29:24 -0300

    se agrego main, provider, outputs, variables tf

---
+++ aws-vpc-terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+}
+
+variable "public_subnet_1" {
+    default = "10.0.0.0/24"
+}
+
+variable "public_subnet_2" {
+    default = "10.0.0.0/24"
+}
+
